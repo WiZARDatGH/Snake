e8d2260455aca4fd7d3318aab178eb01c94253e8
Mustafa Kula <mkmk3806@yahoo.com>
2026-09-20 06:39:48 +0300

    Base app

---
+++ snake.py
@@ -0,0 +1,136 @@
+import time
+import random
+import os
+import sys
+import termios
+import tty
+import select
+
+WIDTH = 21
+HEIGHT = 11
+
+snake = [(11, 6)]
+direction = "+x"
+
+food = (13, 6)
+
+def get_key():
+    if select.select([sys.stdin], [], [], 0)[0]:
+        key = sys.stdin.read(1)
+
+        if key == "\x1b":
+            key += sys.stdin.read(2)
+
+            if key == "\x1b[A":
+                return "UP"
+            elif key == "\x1b[B":
+                return "DOWN"
+            elif key == "\x1b[C":
+                return "RIGHT"
+            elif key == "\x1b[D":
+                return "LEFT"
+
+    return None
+
+old_settings = termios.tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin.fileno())
+
+def move(unit, dx=0, dy=0):
+    old_head = unit[0]
+
+    x = old_head[0] + dx
+    y = old_head[1] + dy
+
+    if x >= WIDTH:
+        x = 0
+    elif x < 0:
+        x = WIDTH - 1
+
+    if y >= HEIGHT:
+        y = 0
+    elif y < 0:
+        y = HEIGHT - 1
+
+    for i in range(len(unit) - 1, 0, -1):
+        unit[i] = unit[i - 1]
+
+    unit[0] = (x, y)
+
+try:
+    while True:
+        key = get_key()
+
+        if key == "UP":
+            direction = "-y"
+        elif key == "DOWN":
+            direction = "+y"
+        elif key == "LEFT":
+            direction = "-x"
+        elif key == "RIGHT":
+            direction = "+x"
+
+        print("\033[H", end="")
+
+        for y in range(HEIGHT):
+            line = ""
+
+            for x in range(WIDTH):
+                if (x, y) == food and (x, y) != snake[0]:
+                    line += "*"
+                elif (x, y) == snake[0]:
+                    line += "@"
+                elif (x, y) in snake and (x, y) != snake[0]:
+                    line += "O"
+                else:
+                    line += "."
+
+            print(line)
+
+        if direction == "+x":
+            move(snake, 1, 0)
+
+            if snake[0] == food:
+                snake.append((food[0]-1, food[1]))
+                food = (
+                    random.randint(1, WIDTH),
+                    random.randint(1, HEIGHT)
+                )
+
+        elif direction == "-x":
+            move(snake, -1, 0)
+
+            if snake[0] == food:
+                snake.append((food[0]+1, food[1]))
+                food = (
+                    random.randint(1, WIDTH),
+                    random.randint(1, HEIGHT)
+                )
+
+        elif direction == "+y":
+            move(snake, 0, 1)
+
+            if snake[0] == food:
+                snake.append((food[0], food[1]-1))
+                food = (
+                    random.randint(1, WIDTH),
+                    random.randint(1, HEIGHT)
+                )
+
+        elif direction == "-y":
+            move(snake, 0, -1)
+
+            if snake[0] == food:
+                snake.append((food[0], food[1]+1))
+                food = (
+                    random.randint(1, WIDTH),
+                    random.randint(1, HEIGHT)
+                )
+
+        time.sleep(0.5)
+
+finally:
+    termios.tcsetattr(
+        sys.stdin,
+        termios.TCSADRAIN,
+        old_settings
+    )
